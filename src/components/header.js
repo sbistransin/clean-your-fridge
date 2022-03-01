edb322e82bde5f894d7cdf996f2e1cf94d215e3b
Samuel Zimel <samuel.c.zimel@gmail.com>
2022-03-01 15:07:53 -0600

    header formatting

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,17 @@
+import '../temporary.css'
 const Header = () => {
   return (
-    <div className="header">
-      <h1>Clean Your Fridge App</h1>
-      <hr />
+    <div>
+      <div 
+      className="header h-spacing">
+        <h1 className="mb-0">Clean Your Fridge App</h1>
+      </div>
+      <div style={{  
+        backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")",
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+}}></div>
     </div>
   )
 }
