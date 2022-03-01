528e393fca061046afe48436c278d1ecc8caad51
Samuel Zimel <samuel.c.zimel@gmail.com>
2022-03-01 12:37:59 -0600

    please

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import FilterReducer from "./reducer-filters";
-import RecipeReducer from './reducer-recipes';
+import recipeReducer from './reducer-recipes';
 
 const rootReducer = combineReducers({
   filters: FilterReducer,
