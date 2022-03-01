9c49b31b179b66ca55625f70fc31479950f24aa5
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-03-01 14:14:52 -0500

    reoranized files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from './components/app'
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from "react-redux";
